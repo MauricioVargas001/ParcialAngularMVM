8665ec050595f4f96f64021619dc33212a78fc4d
Mauricio Vargas <cm.vargasm1@uniandes.edu.co>
2023-11-04 18:40:03 -0400

    Creacion de proyecto de angular de la evaluacion practica de angular

---
+++ src/app/vivero/vivero.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViveroComponent } from './vivero.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ViveroComponent]
+})
+export class ViveroModule { }
